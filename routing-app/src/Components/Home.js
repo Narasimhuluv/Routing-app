3563354283a4e7a225caa72db5e3a0b9cd631b01
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-09-21 17:50:14 +0530

    add files

--- routing-app/src/Components/Home.js
+++ routing-app/src/Components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
         <div className="w-full">
+            <Link to="/articles" exact>
+                <h2 className="border py-1  w-3/12 text-center my-6 rounded-lg bg-green-400 text-white font-bold">Articles</h2>
+            </Link>
             <img  className="w-6/12 m-auto" src="/images/welocme1.gif" alt="" />
         </div>
     )
